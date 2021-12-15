424608f84432ce079112e2045d27aeb8f7c65a52
Ajoy Thomas <ajoythomas@gmail.com>
2021-12-15 03:26:19 +0000

    initial push

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
- from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import requests
 import re
 import pandas as pd
 import time
+import string
+
+#TODO:
+#1. Figure out the number of pages by looking at the URL for the last item in the pagination. e.g. A has 9 pages, S has 11 pages. Do this Dynamically, instead of hardcoding it
+
+#2. The Letter D is giving an error on illegal character, something doesn't play nice with excel. Look into it later... 
 
 
-
-
-
-#Figure out the number of pages by looking at the URL for the last item in the pagination. e.g. A has 9 pages, S has 11 pages.
-
-
-def run_scrapper():
+def run_scrapper(Alphabet, TotalPages):
     print('running')
+    mbl = pd.DataFrame()
+    filename = "Brands-"+ Alphabet +".xlsx"
+   
+
+    brand_list = []
+    category_list =[]
     
-    pages=9
-    for x in range(1, pages+1):
-      url = 'https://www.manualslib.com/brand/A.html'
-      url += '?page='
-      url += str(x)
+    for x in range(1, TotalPages+1):
+      url = "https://www.manualslib.com/brand/"+Alphabet+".html?page="+str(x)
       print(url)
+     
       time.sleep(5)
       content = requests.get(url).text
       soup = BeautifulSoup(content, 'html.parser')
@@ -29,25 +33,53 @@ def run_scrapper():
 
 
       brands = soup.findAll('div', {'class': regexBrand})
-      brand_list = []
+     
       for b in brands[0:]:
         result = b.text.strip()
         brand_list.append(result)
-      print(brand_list)
     
       category = soup.findAll('div',{'class': regexCategory})
-      category_list =[]
+      
       for c in category[0:]:
         result2 = c.text.strip()
         category_list.append(result2)
-
-
-    #mbl = pd.DataFrame({'Brands':brand_list,'Category':category_list})
-
-
-    #mbl.to_excel("Brands.xlsx",index=False)
-    
+      
+      
+    mbl = pd.DataFrame({'Brands':brand_list,'Category':category_list})
+    mbl.to_excel(filename,index=False)
     print("done")
 
+def BrandLoop():
+ 
+ run_scrapper("A",9)
+ run_scrapper("B",6)
+ run_scrapper("C",7)
+# run_scrapper("D",5)
+ run_scrapper("E",6)
+ run_scrapper("F",4)
+ run_scrapper("G",4)
+ run_scrapper("H",5)
+ run_scrapper("I",4)
+ run_scrapper("J",2)
+ run_scrapper("K",3)
+ run_scrapper("L",4)
+ run_scrapper("M",7)
+ run_scrapper("N",4)
+ run_scrapper("O",3)
+ run_scrapper("P",6)
+ run_scrapper("Q",1)
+ run_scrapper("R",4)
+ run_scrapper("S",11)
+ run_scrapper("T",6)
+ run_scrapper("U",2)
+ run_scrapper("V",3)
+ run_scrapper("W",3)
+ run_scrapper("X",1)
+ run_scrapper("Y",1)
+ run_scrapper("Z",1)
 
-run_scrapper()
+  #for alphabet in string.ascii_lowercase:
+    #call scraper for each letter
+
+BrandLoop()
+
